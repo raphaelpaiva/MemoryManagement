2396aaff31112facd134a242fb79d92940707299
Raphael Paiva <raphaeldpaiva@gmail.com>
2012-07-02 17:12:26 -0300

    Creating Memory Class

---
+++ src/br/ufrj/dcc/so/Memory.java
@@ -0,0 +1,22 @@
+package br.ufrj.dcc.so;
+
+
+public class Memory {
+	
+	private Frame[] frames;
+	
+	public Memory(int frameCount) {
+		super();
+		
+		frames = new Frame[frameCount];
+	}
+
+	public Frame[] getFrames() {
+		return frames;
+	}
+
+	public void setFrames(Frame[] frames) {
+		this.frames = frames;
+	}
+	
+}
