216b9a447c2d63e9f30aea297ed7d49648655a79
Raphael Paiva <raphaelpaiva@gmail.com>
2012-07-03 20:12:15 -0300

    Extracting methods for a better readibility.

--- src/br/ufrj/dcc/so/manager/Manager.java
+++ src/br/ufrj/dcc/so/manager/Manager.java
@@ -15,11 +15,11 @@ public class Manager {
 	protected Map<Thread, PageTable> pageTables;
 	protected Map<Thread, Queue<Integer>> requestQueues;
 	protected int maxPageCount;
-	
+
 	public Manager(Memory memory, int maxPageCount) {
 		this.memory = memory;
 		this.maxPageCount = maxPageCount;
-		
+
 		pageTables = new HashMap<Thread, PageTable>();
 		requestQueues = new HashMap<Thread, Queue<Integer>>();
 	}
@@ -31,52 +31,69 @@ public class Manager {
 
 	public void request(Thread thread, int pageNum) {
 		System.out.println(thread + " requested page " + pageNum);
-		
+
 		PageTable pageTable = pageTables.get(thread);
 		Queue<Integer> queue = requestQueues.get(thread);
-		
+
 		Frame pageFrame = pageTable.getPageFrame(pageNum);
-		
+
 		if (pageFrame == null) {
 			if (pageTable.getTotalPagesInMemory() < 3) {
-				try {
-					Frame nextFreeFrame = getNextFreeFrame();
-					nextFreeFrame.setFree(false);
-					pageTable.setPageFrame(nextFreeFrame, pageNum);
-					
-					queue.add(pageNum);
-					
-					System.out.println("Page " + pageNum + " for " + thread + " brought to memory in " + nextFreeFrame);
-				} catch (OutOfMemoryException e) {
-					System.out.println("Out of Memory!");
-				}
+				fetchPage(thread, pageNum, pageTable, queue);
 			} else {
-				Integer lruPageNumber = queue.remove();
-				System.out.println(">>> lruPageNumber: " + lruPageNumber);
-				
-				Frame usedFrame = pageTable.getPageFrame(lruPageNumber);
-				pageTable.removePage(lruPageNumber);
-				pageTable.setPageFrame(usedFrame, pageNum);
-				
-				queue.add(pageNum);
-				
-				System.out.println("Page " + pageNum + " for " + thread + " brought to memory in " + usedFrame + ", replacing Page " +  lruPageNumber );
+				fetchPageLRU(thread, pageNum, pageTable, queue);
 			}
 		} else {
-			queue.remove(pageNum);
-			queue.add(pageNum);
-			System.out.println("Page " + pageNum + " for " + thread + " already in memory");
+			updateUsage(thread, pageNum, queue);
 		}
-		
+
 	}
-	
+
+	private void updateUsage(Thread thread, int pageNum, Queue<Integer> queue) {
+		queue.remove(pageNum);
+		queue.add(pageNum);
+		System.out.println("Page " + pageNum + " for " + thread
+				+ " already in memory");
+	}
+
+	private void fetchPageLRU(Thread thread, int pageNum, PageTable pageTable,
+			Queue<Integer> queue) {
+		Integer lruPageNumber = queue.remove();
+
+		Frame usedFrame = pageTable.getPageFrame(lruPageNumber);
+		pageTable.removePage(lruPageNumber);
+		pageTable.setPageFrame(usedFrame, pageNum);
+
+		queue.add(pageNum);
+
+		System.out.println("Page " + pageNum + " for " + thread
+				+ " brought to memory in " + usedFrame + ", replacing Page "
+				+ lruPageNumber);
+	}
+
+	private void fetchPage(Thread thread, int pageNum, PageTable pageTable,
+			Queue<Integer> queue) {
+		try {
+			Frame nextFreeFrame = getNextFreeFrame();
+			nextFreeFrame.setFree(false);
+			pageTable.setPageFrame(nextFreeFrame, pageNum);
+
+			queue.add(pageNum);
+
+			System.out.println("Page " + pageNum + " for " + thread
+					+ " brought to memory in " + nextFreeFrame);
+		} catch (OutOfMemoryException e) {
+			System.out.println("Out of Memory!");
+		}
+	}
+
 	private Frame getNextFreeFrame() throws OutOfMemoryException {
 		for (Frame frame : memory.getFrames()) {
 			if (frame.isFree()) {
 				return frame;
 			}
 		}
-		
+
 		throw new OutOfMemoryException();
 	}
 }
